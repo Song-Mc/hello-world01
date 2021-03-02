4740f5449d55412c457f6560a362ff6f53f9d301
SongMc <1471485017@qq.com>
2021-03-03 00:04:40 +0800

    测试自动化部署2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
       <p>半夜母猪为何频频惨叫</p>
-      <p>奥儿家的母牛为何频频哭泣</P>
+      <p>XX家的母牛为何频频哭泣</P>
       <p>是道德的沦丧还是人性的扭曲</P>
       <p>敬请关注今晚10点焦点访谈</P>
-      <p>带你走进奥胖之奥儿的不归路~~~</P>
-      <p>呜嗷是个傻狗 嗷呜嗷呜嗷呜~~~</p>
+      <p>带你走进XX之XX的不归路~~~</P>
+   <img src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2571253861,3464212252&fm=26&gp=0.jpg" />
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>
