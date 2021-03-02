182d951c6a59781c355ed6ad24c0110912d3ebd1
Song-Mc <1471485017@qq.com>
2021-03-02 18:18:42 +0800

    first project

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <p>java0821 全体同学 貌美如花</p>
       <p>祝 java0821 全体同学 拿到自己心仪的offer</p>
       <p>祝 java0821 全体同学 拿到自己心仪的offer</p>
+      <p>祝 米西米西花不拉几，如果你要密西我就不要垃圾</p>	  
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>
