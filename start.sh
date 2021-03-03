98ed3b78bd3a43debf8c80761674e2bc3fa70b7f
Song-Mc <75706079+Song-Mc@users.noreply.github.com>
2021-03-03 22:53:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,14 @@ git pull git@github.com:Song-Mc/hello-world01.git
 npm install --registry=https://registry.npm.taobao.org/ && npm run build
 
 # 删除容器
-docker rm -f demo1 &> /dev/null
+# docker rm -f demo1 &> /dev/null
 
 #启动容器
-docker run -d --restart=on-failure:5\
-    -p 8080:80 \
-    -v $PWD/dist:/usr/share/nginx/html \
-    --name demo1 nginx
+#docker run -d --restart=on-failure:5\
+#    -p 8080:80 \
+#   -v $PWD/dist:/usr/share/nginx/html \
+#    --name demo1 nginx
+    
+docker run -d -p 8080:80 -v $PWD/dist:/usr/share/nginx/html --name demo999 nginx    
 
 
